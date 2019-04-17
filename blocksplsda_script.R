fee6eaa145f16312472e1c503b0c57a32762eb09
pierre <pierre.pericard@pasteur-lille.fr>
2019-04-17 15:56:16 +0200

    Add initial R script

---
+++ blocksplsda_script.R
@@ -0,0 +1,50 @@
+#!/usr/bin/env Rscript
+
+# Setup R error handling to go to stderr
+options( show.error.messages=F, error = function () { cat( geterrmessage(), file=stderr() ); q( "no", 1, F ) } )
+
+# we need that to not crash galaxy with an UTF8 error on German LC settings.
+loc <- Sys.setlocale("LC_MESSAGES", "en_US.UTF-8")
+
+## Main Function ##
+
+library(argparse)
+
+parser <- ArgumentParser(description='Run the mixOmics block.splsda function')
+
+parser$add_argument('--block', dest='blocks_list', action="append", required=TRUE, help="Block file")
+parser$add_argument('--samples', dest='samples_file', required=TRUE, help="Samples description file")
+parser$add_argument('--ncomp', dest='ncomp', type='integer', required=TRUE, help="Samples description file")
+
+args <- parser$parse_args()
+
+##
+print(args$blocks_list)
+print(args$samples_file)
+print(args$ncomp)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
