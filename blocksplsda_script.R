ac9ad07bd9be7177731a4a478b9391b4b90b33e8
pierre <pierre.pericard@pasteur-lille.fr>
2019-04-18 13:35:15 +0200

    Compute the correlation design matrix

--- blocksplsda_script.R
+++ blocksplsda_script.R
@@ -51,27 +51,45 @@ for(i in 1:nrow(args$blocks_list))
     list_X[[args$blocks_list[i,1]]] <- read.table(args$blocks_list[i,2], sep='\t', header=TRUE, row.names=1)
 }
 
-print(list_X)
+# print(list_X)
 
 samples_descriptions <- read.table(args$samples_file, sep='\t', header=TRUE, row.names=1)
 
+print("Samples descriptions matrix:")
 print(samples_descriptions)
 
 Y <- factor(samples_descriptions[[1]])
 
+print("Y factor matrix:")
 print(Y)
 
 block_nb <- nrow(args$blocks_list)
 
 design <- matrix(0, nrow = block_nb, ncol = block_nb)
 
+if(args$correlation)
+{
+    design <- matrix(1, nrow = block_nb, ncol = block_nb)
+    diag(design) <- 0
+}
+
+print("Design matrix:")
 print(design)
 
 res_block_splsda <- block.splsda(X = list_X,
                                  Y = Y,
                                  ncomp = args$ncomp,
-                                 design = design)
+                                 design = design,
+                                 scheme = args$scheme,
+                                 mode = args$mode,
+                                 scale = TRUE,
+                                 init = "svd",
+                                 tol = 1e-06,
+                                 max.iter = args$maxiter,
+                                 near.zero.var = FALSE,
+                                 all.outputs = TRUE)
 
+print("block.splsda object:")
 print(res_block_splsda)
 
 save(res_block_splsda, file=args$output_rdata)
