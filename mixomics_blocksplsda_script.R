2d2203ffe5affc2bca62a90ad01c2b4464802113
pierre <pierre.pericard@pasteur-lille.fr>
2019-04-25 15:53:13 +0200

    Added more input and output parameters

--- mixomics_blocksplsda_script.R
+++ mixomics_blocksplsda_script.R
@@ -13,21 +13,25 @@ suppressPackageStartupMessages(require(argparse))
 parser <- ArgumentParser(description='Run the mixOmics block.splsda function')
 
 parser$add_argument('--block', dest='blocks_list', nargs=4, action="append", required=TRUE, help="Block name + nb variables to select + data matrix file + variables metadata file")
-parser$add_argument('--samples', dest='samples_file', required=TRUE, help="Samples description file")
+parser$add_argument('--sample_metadata_in', dest='sample_metadata_in', required=TRUE, help="Samples metadata file")
+parser$add_argument('--sample_description_col', dest='sample_description_col', type='integer', required=TRUE, help="Sample description column number")
 parser$add_argument('--ncomp', dest='ncomp', type='integer', required=TRUE, help="Number of components to include in the model")
 parser$add_argument('--correlation', dest='correlation', action="store_true", help="Add correlation between all blocks")
 parser$add_argument('--scheme', dest='scheme', required=TRUE, help="Scheme")
 parser$add_argument('--mode', dest='mode', required=TRUE, help="Mode")
 parser$add_argument('--maxiter', dest='maxiter', type='integer', required=TRUE, help="Maximum number of iterations")
-parser$add_argument('--outrdata', dest='output_rdata', required=TRUE, help="Output Rdata file")
+parser$add_argument('--rdata_out', dest='rdata_out', required=TRUE, help="Output Rdata file")
+parser$add_argument('--sample_metadata_out', dest='sample_metadata_out', required=TRUE, help="Output sample metadata file")
 
 args <- parser$parse_args()
 
 ##
 print("Blocks:")
 print(args$blocks_list)
-print("Sample description file:")
-print(args$samples_file)
+print("Sample metadata file:")
+print(args$sample_metadata_in)
+print("Sample description column number:")
+print(args$sample_description_col)
 print("Number of components:")
 print(args$ncomp)
 print("Compute correlation between all blocks:")
@@ -39,7 +43,9 @@ print(args$mode)
 print("Max nb of iterations:")
 print(args$maxiter)
 print("Output Rdata file:")
-print(args$output_rdata)
+print(args$rdata_out)
+print("Output sample metadata file:")
+print(args$sample_metadata_out)
 
 # loading libraries
 suppressPackageStartupMessages(require(mixOmics))
@@ -68,12 +74,18 @@ for(i in 1:nrow(args$blocks_list))
 
 # print(list_X)
 
-samples_descriptions <- read.table(args$samples_file, sep='\t', header=TRUE, row.names=1)
+sample_metadata <- read.table(args$sample_metadata_in, sep='\t', header=TRUE, row.names=1)
 
-print("Samples descriptions matrix:")
-print(samples_descriptions)
+print("Sample metadata matrix:")
+print(sample_metadata)
 
-Y <- factor(samples_descriptions[[1]])
+description_column <- ncol(sample_metadata)
+if(args$sample_description_col > 0)
+{
+    description_column <- args$sample_description_col
+}
+
+Y <- factor(sample_metadata[[description_column]])
 
 print("Y factor matrix:")
 print(Y)
@@ -108,4 +120,4 @@ res_block_splsda <- block.splsda(X = list_X,
 print("Block.splsda object:")
 print(res_block_splsda)
 
-save(res_block_splsda, file=args$output_rdata)
+save(res_block_splsda, file=args$rdata_out)
